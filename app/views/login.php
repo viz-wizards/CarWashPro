3c75c231073dd5eee0902564435effd0733bcd21
gOJO4356 <1590173@senati.pe>
2025-05-26 11:24:39 -0500

    Update login.php

--- app/views/login.php
+++ app/views/login.php
@@ -1,15 +1,12 @@
 <?php
-//Escribimos el codigo
 session_start();
 if (isset($_SESSION["usuario_sesion"])) {
     header("Location: views/dashboard.php");
-} else {
-    
+    exit;
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,19 +15,24 @@ if (isset($_SESSION["usuario_sesion"])) {
     <link rel="stylesheet" href="views/assets/css/login.css">
     <!-- Poner icono -->
     <link rel="shortcut icon" href="#" type="image/x-icon"> 
+    <!-- Script reCAPTCHA -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
     <form action="controllers/UserController.php" method="POST" style="border:1px solid;">
         <fieldset>
             <legend>FORMULARIO-EMPRESA</legend>
             <label for="user">Usuario</label>
-            <input type="text" name="user" placeholder="Escribe tu usuario">
+            <input type="text" name="user" placeholder="Escribe tu usuario" required>
+
             <label for="pass">Contraseña</label>
-            <input type="password" name="pass" placeholder="Escribe tu contraseña">
+            <input type="password" name="pass" placeholder="Escribe tu contraseña" required>
+
+            <!-- reCAPTCHA -->
+            <div class="g-recaptcha" data-sitekey="6Lei8EkrAAAAAD7vv-YK22L5U-oZTivGPjrndBmQ"></div>
+
             <input type="submit" value="Iniciar Sesión">
         </fieldset>
     </form>
-
 </body>
-
 </html>
